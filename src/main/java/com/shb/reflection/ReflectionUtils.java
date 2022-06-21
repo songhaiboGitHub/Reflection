c40c80b203718b09e9d856dc165921ab1e0686be
ext.songhaibo <ext.songhaibo@jd.com>
2022-06-21 17:18:38 +0800

    feat: 通过反射获取类的结构信息

--- src/main/java/com/shb/reflection/ReflectionUtils.java
+++ src/main/java/com/shb/reflection/ReflectionUtils.java
@@ -16,7 +16,8 @@ public class ReflectionUtils {
     }
 
     @Test
-    public void api_01() throws ClassNotFoundException, IllegalAccessException {
+//    第一组：java.lang.Class类
+    public void api_01() throws ClassNotFoundException {
         //得到Class对象
         Class<?> personClass = Class.forName("com.shb.reflection.Person");
         //        1. getName：获取全类名
@@ -39,7 +40,7 @@ public class ReflectionUtils {
         for (Method declaredMethod : personClass.getDeclaredMethods()) {
             System.out.println(declaredMethod.getName());
         }
-        //        7. getConstructors：获取所有public修饰的构造器，包含本类以及父类的
+        //        7. getConstructors：获取所有public修饰的构造器，包含本类的
         for (Constructor<?> constructor : personClass.getConstructors()) {
             System.out.println(constructor.getName());
         }
@@ -60,6 +61,41 @@ public class ReflectionUtils {
             System.out.println(annotation);
         }
 
+    }
+
+    @Test
+//    第二组：java.lang.reflect.Field类
+    public void api_02() throws ClassNotFoundException {
+        //得到Class对象
+        Class<?> personClass = Class.forName("com.shb.reflection.Person");
+        for (Field declaredField : personClass.getDeclaredFields()) {
+            //        1. getModifiers：以int形式返回修饰符（有对应的枚举）
+            System.out.println(declaredField.getModifiers());
+            //        2. getType：以Class形式返回类型
+            System.out.println(declaredField.getType());
+            //        3. getName：返回属性名
+            System.out.println(declaredField.getName());
+        }
+    }
+
+    @Test
+//    第三组：java.lang.reflect.Method类
+    public void api_03() throws ClassNotFoundException {
+        //得到Class对象
+        Class<?> personClass = Class.forName("com.shb.reflection.Person");
+        //        6. getDeclaredMethods：获取本类中所有方法
+        for (Method declaredMethod : personClass.getDeclaredMethods()) {
+            //        1. getModifiers：以int形式返回修饰符
+            System.out.println(declaredMethod.getModifiers());
+            //        2. getReturnType：以Class形式获取 返回类型
+            System.out.println(declaredMethod.getReturnType());
+            //        3. getName：返回方法名
+            System.out.println(declaredMethod.getName());
+            //        4. getParameterTypes：以Class[]返回参数类型数组
+            for (Class<?> parameterType : declaredMethod.getParameterTypes()) {
+                System.out.println(parameterType.getName());
+            }
+        }
     }
 }
 
@@ -83,7 +119,7 @@ class Person implements iA, iB {
     private double sar;
 
     //方法
-    public void m1() {
+    public void m1(String A) {
 
     }
 
